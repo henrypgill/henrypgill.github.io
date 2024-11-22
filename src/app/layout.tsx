dccaedd4dad7d28da1bfbaf8c6b3152fba38b4c5
henrypgill <hpgill312@gmail.com>
2024-11-22 16:33:48 +0000

    implemented isolated app header and footer and styled layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Roboto } from "next/font/google";
+import { AppHeader } from "./AppHeader";
+import { AppFooter } from "./AppFooter";
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
 //   variable: "--font-geist-sans",
@@ -28,64 +30,14 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={"font-sans antialiased "}>
+        <html lang="en" className="h-full">
+            <body className={"font-sans antialiased h-full flex flex-col"}>
                 <AppHeader />
-                <main className={roboto.className}>{children}</main>
+                <main className={roboto.className + " flex-grow"}>
+                    {children}
+                </main>
                 <AppFooter />
             </body>
         </html>
-    );
-}
-
-function AppHeader(): JSX.Element {
-    return (
-        <header>
-            <div className="flex flex-row align-middle">
-                <div className="flex flex-col gap-1 p-2">
-                    <div className="bg-black h-[8px] w-[32px] rounded"></div>
-                    <div className="bg-black h-[8px] w-[32px] rounded"></div>
-                    <div className="bg-black h-[8px] w-[32px] rounded"></div>
-                </div>
-                <p>Henry Gill</p>
-            </div>
-        </header>
-    );
-}
-
-function AppFooter(): JSX.Element {
-    return (
-        <footer className="flex bg-black text-white justify-center p-4">
-            <div>
-                <ul className="flex flex-row gap-4 align-middle justify-between w-full">
-                    <li>
-                        <a
-                            href="mailto:hpgill312@gmail.com"
-                            className="text-2xl"
-                        >
-                            email
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://github.com/henrypgill"
-                            className="text-2xl"
-                            target="blank"
-                        >
-                            GitHub
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.linkedin.com/in/henrypgill/"
-                            target="blank"
-                            className="text-2xl"
-                        >
-                            LinkedIn
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </footer>
     );
 }
